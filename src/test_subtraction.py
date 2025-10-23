620bd47fa1a0de2bad3d4ff3461dc8efb7a8d166
HuaBeiPingYuan <yaohua13liluye@gmail.com>
2025-10-23 12:03:14 +0300

    Add subtraction.py with basic tests

---
+++ src/test_subtraction.py
@@ -0,0 +1,13 @@
+from subtraction import subtract
+
+
+def test_subtract_positive():
+    assert subtract(10, 3) == 7
+
+
+def test_subtract_negative():
+    assert subtract(3, 10) == -7
+
+
+def test_subtract_zero():
+    assert subtract(0, 5) == -5
